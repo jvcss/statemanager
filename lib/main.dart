0af0951ee36bbb741a63c919ffd8fd585c4c87e5
jvcss <jvcs.mecatronica@gmail.com>
2024-06-30 12:03:33 -0300

    feat: include list builder

--- lib/main.dart
+++ lib/main.dart
@@ -12,59 +12,69 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const Text( 'Flutter Demo Home Page'),
+      home: const NotesPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-// class NotesPage extends StatefulWidget {
-//   const NotesPage({super.key, required this.title});
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key, required this.title});
 
-//   final String title;
+  final String title;
 
-//   @override
-//   State<NotesPage> createState() => NotesPageState();
-// }
+  @override
+  State<NotesPage> createState() => NotesPageState();
+}
 
-// class NotesPageState extends State<NotesPage> {
-//   int _counter = 0;
+class NotesPageState extends State<NotesPage> {
+  int _counter = 0;
   
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'Variable view managed:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), 
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Variable view managed:',
+            ),
+            ListView.builder(
+              itemCount: ContactBook().length,
+              itemBuilder: (BuildContext context, int index) {
+                final Contact? contact = ContactBook().getContact(index);
+                return ListTile(
+                  title: Text(contact?.name ?? ''),
+                );
+              },
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
