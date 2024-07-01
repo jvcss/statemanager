a87aae8632fddf67d8e53f8463d1c03c83f85058
jvcss <jvcs.mecatronica@gmail.com>
2024-06-30 22:58:23 -0300

    dx: using singleton

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,14 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 3, 195, 105)),
         useMaterial3: true,
       ),
       home: const NotesPage(title: 'Flutter Demo Home Page'),
@@ -33,11 +33,35 @@ class NotesPage extends StatefulWidget {
 
 class NotesPageState extends State<NotesPage> {
   int _counter = 0;
-  
+  TextEditingController controllerNewContact = TextEditingController();
+
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  @override
+  void dispose() {
+    controllerNewContact.dispose();
+    super.dispose();
+  }
+
+  Widget createContact(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: controllerNewContact,
+          decoration: const InputDecoration(
+            labelText: 'Name',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Reset'),
+        ),
+      ],
+    );
   }
 
   @override
@@ -47,34 +71,120 @@ class NotesPageState extends State<NotesPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 100,
+                child: Container(
+                  //padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Theme.of(context).colorScheme.primary),
+                  ),
+                  child: ListView.builder(
+                    itemCount: ContactBook().length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Contact? contact = ContactBook().getContact(index);
+                      return ListTile(
+                        title: Text(
+                          contact?.name ?? '',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            ContactBook().removeContact(contact!);
+                          });
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
             const Text(
               'Variable view managed:',
             ),
-            ListView.builder(
-              itemCount: ContactBook().length,
-              itemBuilder: (BuildContext context, int index) {
-                final Contact? contact = ContactBook().getContact(index);
-                return ListTile(
-                  title: Text(contact?.name ?? ''),
-                );
-              },
-            ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.bodyMedium,
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: controllerNewContact,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+                onSubmitted: (String value) {
+                  setState(() {
+                    ContactBook().addContact(Contact(name: value));
+                    controllerNewContact.clear();
+                  });
+                },
+              ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Create Contact'),
+                content: CreateContact(controllerNewContact: controllerNewContact),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        ContactBook().addContact(Contact(name: controllerNewContact.text));
+                        controllerNewContact.clear();
+                      });
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Create'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
+    );
+  }
+}
+
+class CreateContact extends StatelessWidget {
+  final TextEditingController controllerNewContact;
+  const CreateContact({super.key, required this.controllerNewContact});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: controllerNewContact,
+          decoration: const InputDecoration(
+            labelText: 'Name',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Reset'),
+        ),
+      ],
     );
   }
 }
