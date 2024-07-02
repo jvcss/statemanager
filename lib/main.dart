cda9e076be2033417e34a362d37b725064a3e735
jvcss <jvcs.mecatronica@gmail.com>
2024-07-02 00:51:50 -0300

    understanding the diff between local variable and global variable in stf

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,51 +22,73 @@ class App extends StatelessWidget {
         useMaterial3: true,
       ),
       routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => const HomePage(),
+        '/': (BuildContext context) => const HomePage(
+            //   title: 'Page',
+            ),
       },
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  //final String title;
+  const HomePage({
+    super.key,
+    //this.title = 'Home Page',
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = 'Home Page';
+
+  void _changeTitle() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        title = DateTime.now().toLocal().toString();
+      });
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    _changeTitle();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         centerTitle: true,
-        title: const Text('Home'),
+        title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Material(
-              color: Theme.of(context).colorScheme.inverseSurface,
-              elevation: 7,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  'Home Page',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onInverseSurface,
-                    fontSize: 24,
-                  ),
+      body: GestureDetector(
+        onTap: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
+              duration: const Duration(seconds: 2),
             ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                  labelText: 'Enter your username',
-                  //hintText: 'Enter your name',
-                ),
+          );
+        },
+        child: Container(
+          color: Theme.of(context).colorScheme.primary,
+          child: Center(
+            child: Text(
+              'Go to Second Page',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
