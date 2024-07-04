f8178003e0fdf8f6f2b12b9713a692f73ef95f86
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 13:04:37 -0300

    dx: use memoized strat

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:statemanager/extensions.dart';
+
+const url = 'https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return () => controller.dispose();
-    }, [controller]);
+    final image = useMemoized(
+      () => NetworkAssetBundle(Uri.parse(url))
+          .load(
+            url,
+          )
+          .then((
+            data,
+          ) =>
+              data.buffer.asUint8List())
+          .then(
+            (
+              data,
+            ) =>
+                Image.memory(
+              data,
+            ),
+          ),
+    );
+
+    final snap = useFuture(image);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            TextField(
-              controller: controller,
-              decoration: const InputDecoration(
-                hintText: 'Enter your name',
-              ),
-            ),
-            Text(
-              'Hello, ${text.value}',
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Home Page'),
         ),
-      ),
-    );
+        body: Column(
+          children: [snap.data].compactMap().toList(),
+        ));
   }
 }
