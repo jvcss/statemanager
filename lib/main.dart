67bb2efe9f431a65cd31101842410a49c42f2d55
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 16:27:21 -0300

    dx: clean and add hook

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:statemanager/blocs/person_bloc.dart';
-import 'package:statemanager/screens/page_screen.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   runApp(const App());
@@ -18,12 +16,36 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      routes: {
-        '/': (context) => BlocProvider(
-              create: (_) => PersonBloc(),
-              child: const PageScreen(),
+      home: const Page(),
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  const Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter Demo Home Page'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Variable view managed:',
             ),
-      },
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
