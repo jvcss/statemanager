4594fe42f57845a7833989efcedfca420a1e7b91
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 13:29:20 -0300

    dx: using listenable and memo

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,35 +1,22 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:statemanager/extensions.dart';
+import 'package:statemanager/countdown.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final CountDown counter = useMemoized(() => CountDown(from: 20));
+    final notify = useListenable(counter);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
+      body: Text(
+        'Countdown: ${notify.value.toString()}',
+        style: const TextStyle(fontSize: 24),
+      ),
     );
-  }
-}
-
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription subscription;
-  CountDown({required int from}) : super(from) {
-    subscription = Stream.periodic(
-      const Duration(seconds: 1),
-      (i) => from - i,
-    )
-        .takeWhile(
-          (value) => value >= 0,
-        )
-        .listen(
-          (event) => value = event,
-        );
   }
 }
