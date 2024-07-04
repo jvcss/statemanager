983e122a07519c7a18dfbf4f97ade65e1f5e21cc
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 14:27:50 -0300

    dx: cls

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,43 +10,11 @@ class HomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-    controller = useStreamController(
-      onListen: () {
-        controller.sink.add(0.0);
-      },
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: StreamBuilder<double>(
-          stream: controller.stream,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('An error occurred'),
-              );
-            } else if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            final rotation = snapshot.data ?? 0.0;
-            return GestureDetector(
-              onTap: () {
-                controller.sink.add(rotation + 10);
-              },
-              child: RotationTransition(
-                turns: AlwaysStoppedAnimation(rotation/360),
-                child: Center(
-                  child: Image.network(url),
-                ),
-              ),
-            );
-          }),
+     
     );
   }
 }
