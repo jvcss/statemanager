e2663fe2c4067d6f071e3563bff1565ec26123bc
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 09:22:12 -0300

    dx  using hook stateless

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:statemanager/streaming.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
+    final controller = useTextEditingController();
+    final text = useState('');
+    useEffect(() {
+      controller.addListener(() {
+        text.value = controller.text;
+      });
+      return () => controller.dispose();
+    }, []);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
       body: Center(
-        child: Text('${dateTime.data ?? ''} ?? Loading...'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                hintText: 'Enter your name',
+              ),
+            ),
+            Text(
+              'Hello, ${text.value}',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/streaming.dart
+++
@@ -1,2 +0,0 @@
-
-Stream<String> getTime() => Stream.periodic(const Duration(seconds: 1), (i) => DateTime.now().toLocal().toString());
