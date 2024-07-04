ba1495380355a66b67c77d6aea10a4c83eadc83c
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 14:08:27 -0300

    dx: using hooks for animations. good to notice that sliver can achieve similar results...

--- lib/extensions.dart
+++
@@ -1,8 +0,0 @@
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(
-        transform ?? (e) => e,
-      ).where((element) => element != null).cast();
-}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,21 +1,85 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:statemanager/countdown.dart';
+
+const url = 'https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg';
+
+const imageHeight = 300.0;
+
+extension Normalize on num {
+  num normalized(
+    num selfRangeMin,
+    num selfRangeMax, [
+    num normalizedRangeMin = 0,
+    num normalizedRangeMax = 1,
+  ]) =>
+      (normalizedRangeMax - normalizedRangeMin) * ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) + normalizedRangeMin;
+}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final CountDown counter = useMemoized(() => CountDown(from: 20));
-    final notify = useListenable(counter);
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+    final size = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+
+    final controller = useScrollController();
+    useEffect(() {
+      controller.addListener(() {
+        final newOpacity = max(imageHeight - controller.offset, 0);
+        final normalized = newOpacity.normalized(0, imageHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+
+      return null;
+    }, [controller]);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Text(
-        'Countdown: ${notify.value.toString()}',
-        style: const TextStyle(fontSize: 24),
+      body: Column(
+        children: [
+          SizeTransition(
+            axis: Axis.vertical,
+            sizeFactor: size,
+            axisAlignment: -1.0,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                height: imageHeight,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: controller,
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
