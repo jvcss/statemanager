292e5d4996235f5460f43d80f0ec6cfc93783b3d
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 15:35:53 -0300

    dx: clean rp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:statemanager/api.dart';
-import 'package:statemanager/display_dynamic_view.dart';
-import 'package:statemanager/lights_snackbar.dart';
-import 'package:statemanager/provider.dart';
 
 void main() {
   runApp(const App());
@@ -13,68 +6,36 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
-  final Color green = const Color.fromRGBO(1, 204, 160, 1);
-  final Color backgroundColor = const Color.fromRGBO(26, 31, 36, 1);
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'State Manager 101',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: green),
-        //brightness: Brightness.dark,
+      title: 'Flutter Demo',
+      theme: ThemeData(        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => ApiProvider(
-              api: API(),
-              child: const HomePage(),
-            ),
-      },
+      home: const Page(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  //final String title;
-  const HomePage({
-    super.key,
-    //this.title = 'Home Page',
-  });
+class Page extends StatefulWidget {
+  const Page({super.key, required this.title});
+
+  final String title;
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<Page> createState() => PageState();
 }
 
-// poderia ser usado dentro do widget HomePage
-// mas para fins de demonstração, foi colocado aqui
-// para mostrar que o estado do provider é preservado
-void _changeTitle(BuildContext context) async {
-  Timer.periodic(const Duration(seconds: 3), (timer) {
-    //setState(() {
-      //title = DateTime.now().toLocal().toString();
-      final api = ApiProvider.of(context)!;
-      // essa é a chamada que altera o estado do provider, que consequentemente atualiza o widget DisplayDynamicView
-      // e que por consequência atualiza o widget HomePage
-      // nao existe outra chamada além dessa que altera o estado do provider com um metodo
-      // no widget DisplayDynamicView, apenas a referencia da variavel é diretamente atribuida ao widget
-      // isso significa que o compartilhamento de context herdado por StatelessWidget e InheritedWidget do provider
-      // a chamada da api é feita indiretamente e mesmo assim o valor é atualizado
-      // uma boa demonstração de conservação de estado
-      var dateAndTime = api?.api.getDateAndTime();
-      //_textKey = ValueKey<String?>(api!.id);
+class PageState extends State<Page> {
+  int _counter = 0;
+  
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
     });
-}
-
-class _HomePageState extends State<HomePage> {
-  //ValueKey _textKey = const ValueKey<String?>(null);
-  String title = 'Home Page';
-
-  @override
-  void initState() {
-    super.initState();
-    _changeTitle(context);
   }
 
   @override
@@ -82,59 +43,27 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        centerTitle: true,
-        title: Text(
-          ApiProvider.of(context)?.api.dateAndTime ?? title,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Variable view managed:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          DisplayDynamicView(
-              //   key: _textKey,
-              ),
-          GestureDetector(
-            onTap: () async {
-              showFancySnackbar(context);
-              //final api = ApiProvider.of(context);
-              // final dateAndTime = await api?.api.getDateAndTime(); // this is not so good. why?
-              //because you need to reexcute the function to get the value
-              // if its a heavy function, it will be a problem
-              // most important if the business logic runs over a single instance
-              // you can compromise the list of executions.
-
-              setState(() {
-                //title = ApiProvider.of(context)?.api.dateAndTime ?? 'Loading.';
-                //_textKey = ValueKey<String?>(dateAndTime);
-              });
-            },
-            child: Container(
-              color: Theme.of(context).colorScheme.onBackground,
-              child: Center(
-                child: Text(
-                  'Show case',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.background,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
     );
-  }
-
-  void showFancySnackbar(BuildContext context) {
-    const snackBar = SnackBar(
-      padding: EdgeInsets.zero,
-      clipBehavior: Clip.none,
-      duration: Duration(seconds: 2),
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      content: LightsSnackbarContent(),
-    );
-
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,34 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:statemanager/main.dart';
 
 void main() {
   testWidgets('Add Contact Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( App());
-
-    // Verify that the initial screen is an empty list.
-    expect(find.text('No contacts found'), findsOneWidget);
-    expect(find.text('something'), findsNothing);
-
-    // Tap the '+' icon and open a new page.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle(); // Aguarda a animação e a navegação completa.
-
-    expect(find.text('Name'), findsOneWidget);
-    expect(find.byTooltip('Create'), findsOneWidget);
-
-    // interact with the new page to fulfil the test contact name
-    await tester.enterText(find.byType(TextField), 'something');
-    await tester.pumpAndSettle();
-    await tester.tap(find.byIcon(Icons.plus_one));
-    await tester.pumpAndSettle();
-
-    // Verify that our counter has incremented.
-    expect(find.text('No contacts found'), findsNothing);
-    expect(find.text('something'), findsOneWidget);
+    await tester.pumpWidget(const App());
   });
 }
