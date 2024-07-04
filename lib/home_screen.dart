f31d7d602f9c130aaa9fabd3d22bb2efebde63a1
jvcss <jvcs.mecatronica@gmail.com>
2024-07-04 13:20:39 -0300

    dx: good practices

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,42 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:statemanager/extensions.dart';
-
-const url = 'https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final image = useMemoized(
-      () => NetworkAssetBundle(Uri.parse(url))
-          .load(
-            url,
-          )
-          .then((
-            data,
-          ) =>
-              data.buffer.asUint8List())
-          .then(
-            (
-              data,
-            ) =>
-                Image.memory(
-              data,
-            ),
-          ),
-    );
-
-    final snap = useFuture(image);
-
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
-        body: Column(
-          children: [snap.data].compactMap().toList(),
-        ));
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
+
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription subscription;
+  CountDown({required int from}) : super(from) {
+    subscription = Stream.periodic(
+      const Duration(seconds: 1),
+      (i) => from - i,
+    )
+        .takeWhile(
+          (value) => value >= 0,
+        )
+        .listen(
+          (event) => value = event,
+        );
   }
 }
