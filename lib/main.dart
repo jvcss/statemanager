07d06d8e2239d28dc83bf166019d5fc549467366
jvcss <jvcs.mecatronica@gmail.com>
2024-07-12 19:16:27 -0300

    dx: clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:statemanager/api/login/login_interface.dart';
-import 'package:statemanager/api/note/notes_api_interface.dart';
-import 'package:statemanager/bloc/app/app_bloc.dart';
-import 'package:statemanager/model/login_model.dart';
-import 'package:statemanager/page/login/login_page.dart';
-import 'package:statemanager/page/login/login_stateless.dart';
 
 void main() {
   runApp(const App());
 }
 
 class App extends StatelessWidget {
-  final bool showSliver;
-  const App({
-    super.key,
-    this.showSliver = true,
-  });
-
+  const App({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      title: 'Flutter Demo',
+      theme: ThemeData(        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      routes: {
-        '/': (context) {
-          return BlocProvider(
-            create: (context) {
-              return AppBloc(
-                loginApi: LoginApi(),
-                notesApi: NotesApi(),
-                acceptableLoginModel: const LoginModel.adminAccount(),
-              );
-            },
-            child: showSliver ? const LoginPageVanilla() : const LoginPage(),
-          );
-        },
-      },
+      home: const Page(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Page extends StatefulWidget {
+  const Page({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Page> createState() => PageState();
+}
+
+class PageState extends State<Page> {
+  int _counter = 0;
+  
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Variable view managed:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
     );
   }
 }
