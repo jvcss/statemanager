435de42dc42528785c0bff41ca8b572b2e01ba8b
jvcss <jvcs.mecatronica@gmail.com>
2024-07-14 16:18:17 -0300

    dx: improve view

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget, useTextEditingController;
+import 'package:statemanager/bloc/app/app_bloc.dart';
+import 'package:statemanager/bloc/app/app_event.dart';
 import 'package:statemanager/extensions/if_debug.dart';
+import 'dart:developer' as developer;
 
 class LoginView extends HookWidget {
   const LoginView({super.key});
@@ -15,37 +19,66 @@ class LoginView extends HookWidget {
     );
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log in'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300.0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                'assets/images/logo.jpg',
+                fit: BoxFit.cover,
               ),
             ),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          final email = emailController.text;
+                          final password = passwordController.text;
+                          developer.log('Email: $email, Password: $password');
+
+                          context.read<AppBloc>().add(AppEventLogin(email: email, password: password));
+                        },
+                        child: const Text('Log in'),
+                      ),
+                      const SizedBox(height: 16),
+                      TextButton(
+                        onPressed: () {
+                          context.read<AppBloc>().add(
+                                const AppEventGoToRegistration(),
+                              );
+                        },
+                        child: const Text('Create account'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                final email = emailController.text;
-                final password = passwordController.text;
-                print('Email: $email, Password: $password');
-              },
-              child: const Text('Log in'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
