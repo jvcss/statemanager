0232bc461561144d492d560ef304f4ef1955f06d
jvcss <jvcs.mecatronica@gmail.com>
2024-07-15 09:37:31 -0300

    dx: clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:statemanager/page/app.dart';
-
-import 'firebase_options.dart';
-
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseApp app = await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  debugPrint('Initialized default app ${app.isAutomaticDataCollectionEnabled}');
   runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
 }
