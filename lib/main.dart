111a1209cb9c2dc16f5be360b49b7b5f7fb9f058
jvcss <jvcs.mecatronica@gmail.com>
2025-09-13 15:20:31 -0300

    feat: add game animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,157 @@
-import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:statemanager/home.dart';
-import 'package:rive/rive.dart' as rive;
+import 'package:rive_native/rive_native.dart' as rive;
 
-void main()async  {
+import 'demos/hero_demo.dart';
+import 'examples/examples.dart';
+import 'app.dart';
+
+const _appBarColor = Color(0xFF323232);
+const _backgroundColor = Color(0xFF1D1D1D);
+const _primaryColor = Color(0xFF57A5E0);
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  unawaited(rive.RiveFile.file("assets/hero.riv"));
-  runApp(const App());
+  await rive.RiveNative.init();
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
-        useMaterial3: true,
+      // showPerformanceOverlay: true,
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: _backgroundColor,
+        appBarTheme: const AppBarTheme(backgroundColor: _appBarColor),
+        colorScheme: ColorScheme.fromSwatch().copyWith(primary: _primaryColor),
       ),
-      home: const SimpleAssetAnimation()// const HomePage(title: 'BI Visuals'),
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
     );
   }
 }
 
+class Demo {
+  final Widget widget;
+  final String text;
 
-class SimpleAssetAnimation extends StatelessWidget {
-  const SimpleAssetAnimation({Key? key}) : super(key: key);
+  Demo(this.widget, this.text);
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Demo> widgets = [
+    Demo(const HeroDemo(), "GDC Hero"),
+    // Demo(const HunterXDemo(), "Hunter X Demo"),
+    // Demo(const ExampleBasic(), "Basic"),
+    // Demo(const StabilityTest(), "Stability Test"),
+    // Demo(const ExampleDataBinding(), "Data Binding - Basics"),
+    // Demo(const ExampleDataBindingArtboards(), "Data Binding - Artboards"),
+    // Demo(const ExampleDataBindingImages(), "Data Binding - Images"),
+    // Demo(const ExampleEvents(), "Events"),
+    // Demo(const ExampleTextRuns(), "Updating Text Runs"),
+    // Demo(const ExampleTextRunsNested(), "Updating Nested Text Runs"),
+    // Demo(const StateMachineNestedInputsExample(), "Nested Inputs"),
+    // Demo(const ExampleTickerMode(), "Ticker Mode"),
+    // Demo(const ExampleResponsiveLayout(), "Responsive Layout"),
+    // Demo(const ExampleOutOfBandAssets(), "Out of band assets"),
+    // Demo(const ExampleTickerMode(), "Ticker Mode"),
+    // Demo(const ExampleTimeDilation(), "Time Dilation"),
+    // Demo(const ExampleHitTestBehaviour(), "Hit test behaviour"),
+    // Demo(const ExampleArtboardDoesNotExists(), "Artboard does not exists"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Simple Animation'),
+        title: const Text("Rive Native Examples"),
       ),
-      body: const Center(
-        child: rive.RiveAnimation.asset(
-          'assets/hero.riv',
-          fit: BoxFit.cover,
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: widgets.length,
+              itemBuilder: (context, index) {
+                return _button(
+                  widgets[index].text,
+                  () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return BasePage(child: widgets[index].widget);
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RiveExampleApp.isRiveRender
+                  ? const Text("Active Renderer: Rive")
+                  : const Text("Active Renderer: Flutter (Skia or Impeller)")),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      RiveExampleApp.isRiveRender = true;
+                    });
+                  },
+                  child: const Text("Rive Renderer"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      RiveExampleApp.isRiveRender = false;
+                    });
+                  },
+                  child: const Text("Flutter Renderer"),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _button(String title, VoidCallback onPressed) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+            child: ElevatedButton(onPressed: onPressed, child: Text(title))));
+  }
+}
+
+class BasePage extends StatelessWidget {
+  const BasePage({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(),
+      body: Center(
+        child: Center(child: child),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 //import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:statemanager/main.dart';
+//import 'package:statemanager/main.dart';
 
 void main() {
   testWidgets('View Graph', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    //await tester.pumpWidget(const App());
     // Verify 
   });
 }
